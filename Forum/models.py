41ab3a18a698888a7cb9b9ae21722015b0375919
Delarmelina <felipeferreiradelarmelina@gmail.com>
2021-10-30 10:11:42 -0300

    Banco de Dados Post

--- Forum/models.py
+++ Forum/models.py
@@ -1,3 +1,14 @@
+from ckeditor_uploader.fields import RichTextUploadingField
+from django.contrib.auth.models import User
 from django.db import models
 
-#teste felipe
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    keyWords = models.CharField(max_length=255)
+    content = RichTextUploadingField()
+    created_at = models.DateField(auto_now_add=True)
+    update_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.title
