2fe160d234c8b239c6370395c4cf5d2600fd4d91
Delarmelina <felipeferreiradelarmelina@gmail.com>
2021-11-03 10:27:03 -0300

    Função ADD Post pelo Site criada (sem css)

--- AutvixForum/urls.py
+++ AutvixForum/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('posts/<int:post_id>', views.post),
+    path('newPost/', views.newPost, name='new-post'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Forum/views.py
+++ Forum/views.py
@@ -1,12 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import PostForm
 
 from Forum.models import Post
 
 # Create your views here.
 def home(request):
-    posts = Post.objects.all()
+    posts = Post.objects.all().order_by('-created_at')
     return render(request, './home.html', {'posts': posts})
 
 def post(request, post_id):
     post = Post.objects.get(pk=post_id)
     return render(request, './post.html', {'post': post})
+
+def newPost(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('/')
+    else:
+        form = PostForm()
+        return render(request, './newPost.html', {'form': form})
